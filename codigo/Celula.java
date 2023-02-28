48c0b7d1efa1eb1dd15fd1d9dfa7e751d922acbb
Henrique Almeida <hcarvalhoalmeida7@gmail.com>
2023-02-28 16:46:43 -0300

    Javadoc de Lista e Célula #4

--- codigo/Celula.java
+++ codigo/Celula.java
@@ -1,7 +1,15 @@
+/**
+ * Classe que auxilia a lista de produtos
+ *
+ * @author Henrique Almeida
+ */
 public class Celula {
 	Produto item;
 	Celula anterior;
 
+	/**
+	 * Construtor da classe que cria a celula sentinela
+	 */
 	Celula(Produto item, Celula anterior) {
 		this.item = item;
 		this.anterior = anterior;
--- codigo/Lista.java
+++ codigo/Lista.java
@@ -1,18 +1,38 @@
+/**
+ * Classe que representa uma lista de produtos
+ *
+ * @author Henrique Almeida
+ */
 public class Lista {
 	private Celula topo;
 
+	/**
+	 * Construtor da classe que cria uma lista com o topo como sentinela
+	 */
 	Lista() {
-		this.topo = new Celula(null,null);
+		this.topo = new Celula(null, null);
 	}
 
+	/**
+	 * Insere um novo produto na lista
+	 *
+	 * @param novo_item nome do novo produto
+	 * @throws Exception caso o produto ja exista
+	 */
 	void inserir(Produto novo_item) throws Exception {
-		if (get_referencia_cel(novo_item.getDesc()) != null)
+		if (get_referencia_cel(novo_item.getDesc()) != null) // verifica se o produto ja existe
 			throw new Exception("\n Erro, produto ja existente");
-		this.topo.anterior = new Celula(novo_item, this.topo.anterior);
+		this.topo.anterior = new Celula(novo_item, this.topo.anterior); // insere antecessor ao topo sentinela, passando
+																		// o antigo como anterior do novo item
 	}
 
+	/**
+	 * Printa as informacoes de todos os produtos
+	 *
+	 */
 	void listar() {
-		for (Celula tmp = this.topo.anterior; tmp != null; tmp = tmp.anterior)
+		for (Celula tmp = this.topo.anterior; tmp != null; tmp = tmp.anterior) // percorre a lista em ordem contraria a
+																				// de insercao
 			tmp.item.print_produto();
 	}
 }
